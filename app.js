5a2d13977fa1edcd39b00a9e557d77f9209d6290
vamsreddy <baddam.reddy@kellton.com>
2023-10-06 11:47:46 +0530

    Day3-nodejs-learning

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRouter');
+const userRouter = require('./routes/userRouter');
+
+const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
+/////////////////////////MIDDLEWARES
+app.use((req, res, next) => {
+  console.log('Hello from Middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+////////////////////////////////////////////////ROUTE
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+exports.getAllUsers =(req, res)=>{
+    res.status(500).json({
+        status : 'error',
+        message : 'This route is not ready'
+    });
+};
+exports.getUser =(req, res)=>{
+    res.status(500).json({
+        status : 'error',
+        message : 'This route is not ready'
+    });
+};
+exports.createUser =(req, res)=>{
+    res.status(500).json({
+        status : 'error',
+        message : 'This route is not ready'
+    });
+};
+exports.updateUser =(req, res)=>{
+    res.status(500).json({
+        status : 'error',
+        message : 'This route is not ready'
+    });
+};
+exports.deleteUser =(req, res)=>{
+    res.status(500).json({
+        status : 'error',
+        message : 'This route is not ready'
+    });
+};
