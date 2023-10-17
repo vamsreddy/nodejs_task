8e1b3790d9abf2422be595ac2965b263682a232b
vamsreddy <baddam.reddy@kellton.com>
2023-10-17 16:40:26 +0530

    Day11-nodejs-learning

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,10 +15,16 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 const reviewRouter = require('./routes/reviewRouter');
+const viewRouter = require('./routes/viewRouter');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //////////GLOBAL MIDDLEWARES
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(helmet());
 
@@ -52,15 +59,13 @@ app.use(
   }),
 );
 
-app.use(express.static(`${__dirname}/public`));
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 ///////////////////ROUTE
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
@@ -70,4 +75,5 @@ app.all('*', (req, res, next) => {
 });
 
 app.use(globalErrorHandler);
+
 module.exports = app;
